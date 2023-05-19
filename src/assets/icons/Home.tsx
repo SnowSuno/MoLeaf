83deeb69861d8367aeeb5e13189e9d70c40d74e4
Nicole Lee <nicolelee2001@gmail.com>
2023-05-20 03:41:14 +0900

    :sparkles: Implement navigation bar

---
+++ src/assets/icons/Home.tsx
@@ -0,0 +1,20 @@
+import { icon } from "./utils";
+
+export const Home = icon(({ color, size, weight }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="none"
+    width={size}
+    height={size}
+  >
+    <path
+      fill={color}
+      stroke={color}
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width={weight}
+      d="M20 19v-8.5a1 1 0 0 0-.4-.8l-7-5.25a1 1 0 0 0-1.2 0l-7 5.25a1 1 0 0 0-.4.8V19a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1z"
+    />
+  </svg>
+));
