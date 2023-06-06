e6c035ff9c5a1f8dc495d6dfbf7dca218d850c92
Nicole Lee <nicolelee2001@gmail.com>
2023-06-07 07:39:36 +0900

    Add more translations

--- src/components/blocks/AppUsage.tsx
+++ src/components/blocks/AppUsage.tsx
@@ -3,20 +3,24 @@ import { Text } from "~/components/elements/Text";
 
 import { DailyUsageRequired } from "~/types";
 import styled from "@emotion/styled";
+import { useTranslation } from "react-i18next";
 
 interface Props {
   data: DailyUsageRequired<"totalTime" | "maxTime" | "avgTime">;
 }
 
 export const AppUsage: React.FC<Props> = ({ data }) => {
+  const { t } = useTranslation();
 
   return (
     <div>
-      <Text>앱별 사용량</Text>
+      <Text>{t(`common.appUsage`)}</Text>
       <Container>
-        {data.usageData.details.map(({ appName, usage }) =>
-          <div key={appName}>{appName}: {usage}</div>
-        )}
+        {data.usageData.details.map(({ appName, usage }) => (
+          <div key={appName}>
+            {appName}: {usage}
+          </div>
+        ))}
       </Container>
     </div>
   );
@@ -26,12 +30,12 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 1px;
-  
+
   border-radius: 12px;
   background: var(--light-gray);
-  
+
   overflow: hidden;
-  
+
   & > div {
     background: var(--background-color);
     padding: 10px;
